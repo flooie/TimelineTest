b9a9796afcde0dd96b67b999dfe68c96370fb058
William Palin <flooie@users.noreply.github.com>
2019-09-18 15:06:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,19 +6,17 @@ $.getJSON("flp.json", function( data ) {
     google.charts.setOnLoadCallback(drawChart);
     
       function drawChart() {
-    var container = document.getElementById('timeline');
-    var chart = new google.visualization.Timeline(container);
-    var dataTable = new google.visualization.DataTable();
-    dataTable.addColumn({ type: 'string', id: 'Term' });
-    dataTable.addColumn({ type: 'string', id: 'Name' });
-    dataTable.addColumn({ type: 'date', id: 'Start' });
-    dataTable.addColumn({ type: 'date', id: 'End' });
-    dataTable.addRows([
-      [ '1', 'George Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-      [ '2', 'John Adams',        new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-      [ '3', 'Thomas Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ]]);
 
-    var options = {
+            var container = document.getElementById('timeline');
+            var chart = new google.visualization.Timeline(container);
+            var dataTable = new google.visualization.DataTable();
+
+              for (var i in data) {
+                console.log(data[i])
+              }
+          
+          
+          var options = {
       timeline: { showRowLabels: false }
     };
 
